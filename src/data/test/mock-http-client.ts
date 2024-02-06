565fd838b5d57aee632bba0bef76307564a99eb3
andre.borba <dev_andre.filipe@outlook.com>
2024-02-05 22:48:43 -0300

    refactor: lint fixes

--- src/data/test/mock-http-client.ts
+++ src/data/test/mock-http-client.ts
@@ -1,16 +1,18 @@
-import { HttpPostClient, HttpPostParams } from "@/data/protocols/http/http-post-client"
-import { HttpResponse, HttpStatusCode } from "@/data/protocols/http/http-response"
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { HttpPostClient, HttpPostParams } from '@/data/protocols/http/http-post-client'
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { HttpResponse, HttpStatusCode } from '@/data/protocols/http/http-response'
 
 export class HttpPostClientSpy implements HttpPostClient {
-    url?: string
-    body?: object
+  url?: string
+  body?: object
     response: HttpResponse = {
-        statusCode: HttpStatusCode.noContent
+      statusCode: HttpStatusCode.noContent
     }
 
-    async post(params: HttpPostParams): Promise<HttpResponse> {
-        this.url = params.url
-        this.body = params.body
-        return Promise.resolve(this.response)
+    async post (params: HttpPostParams): Promise<HttpResponse> {
+      this.url = params.url
+      this.body = params.body
+      return Promise.resolve(this.response)
     }
 }
